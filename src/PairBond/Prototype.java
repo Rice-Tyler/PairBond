4b490dc85f5de39db5b4ee9e8801dbf6af8afd26
zm2fc <40801861+zm2fc@users.noreply.github.com>
2018-07-12 21:28:30 -0400

    Add files via upload

--- src/PairBond/Prototype.java
+++ src/PairBond/Prototype.java
@@ -79,6 +79,9 @@ public class Prototype extends Game {
 	int Switch_count = 19;
 	boolean end = false;
 	String Winner ="";
+	String Ending_Instructions = "Please Exit and Restart the";
+	String Ending_Instructions2 = "Game to Play Again!!!";
+	//String indented_instructions = Ending_Instructions.replaceAll("(?m)^", "\t");
 	
 	ArrayList<DisplayObject> PlayerSelect = Tanks.getChildren();
 	public Prototype() {
@@ -110,7 +113,16 @@ public class Prototype extends Game {
 		    }
 		}
 //		tank1.setyMax(100);
-		
+		SM.loadMusic("firstscreen", "resources/LoadingScreenBackgroundSound.wav");
+		SM.loadMusic("fieldmusic", "resources/tankWarBackgroundSound.wav");
+		SM.loadMusic("desertmusic", "resources/TankDesertTheme.wav");
+		SM.loadMusic("wintermusic", "resources/TankWinterTheme.wav");
+		SM.loadSoundEffect("MenuSelect", "resources/MenuSelectSound.wav");
+		SM.loadSoundEffect("GO", "resources/TankGO.wav");
+		SM.loadSoundEffect("Victory", "resources/TankWarVictory.wav");
+		SM.loadSoundEffect("move", "resources/TankMovement1.wav");
+		SM.loadSoundEffect("Aim", "resources/TankAim.wav");
+		SM.loadSoundEffect("platdestroyed", "resources/PlatformDestroyed2.wav");
 //		 SM.loadSoundEffect("jump", "resources/jump.wav");
 //		SM.loadMusic("music","resources/mario_music.wav");
 //		this.dispatchEvent(new SoundEvent(SoundEvent.TRIGGER_MUSIC,this,"music"));
@@ -135,19 +147,22 @@ public class Prototype extends Game {
 			Tank currPlayer = (Tank)PlayerSelect.get(player);
 			if(!pause_movement) {
 				if(pressedKeys.contains(KeyEvent.VK_LEFT)) {
-					
+					SM.playSoundEffect("move");
 					currPlayer.move = true;
 					currPlayer.push_force(-2,0);
 				}
 				if(pressedKeys.contains(KeyEvent.VK_RIGHT)) {
+					SM.playSoundEffect("move");
 					currPlayer.move = true;
 					currPlayer.push_force(2,0);
 				}
 				if(pressedKeys.contains(KeyEvent.VK_Q) && currPlayer.angle < 270) {
+					SM.playSoundEffect("Aim");
 					currPlayer.angle+=.8;
 					currPlayer.setGunRotation(currPlayer.getGunRotation()-Math.toRadians(.8));
 				}
 				if(pressedKeys.contains(KeyEvent.VK_W) && currPlayer.angle > 90) {
+					SM.playSoundEffect("Aim");
 					currPlayer.angle-=.8;
 					currPlayer.setGunRotation(currPlayer.getGunRotation()+Math.toRadians(.8));
 				}
@@ -163,7 +178,6 @@ public class Prototype extends Game {
 				Switch = true;
 			}
 			else Switch_count++;
-	
 			
 			if(pressedKeys.contains(KeyEvent.VK_SPACE) && fire) {
 				fire = false;
@@ -220,6 +234,7 @@ public class Prototype extends Game {
 				}
 				for(int m = 0; m < lv.getNumPlatforms(); m++) {
 					if(P.collidesWith(lv.getPlatform(m)) && !explode) {
+						//SM.playSoundEffect("platdestroyed");
 						this.dispatchEvent(new ProjectileEvent(ProjectileEvent.PROJECTILE_EXPLODE,P,P.getId(),"s"));
 						explode = true;
 						x--;
@@ -227,6 +242,7 @@ public class Prototype extends Game {
 				}
 				for(int n = 0; n < lv.getNumDestructables(); n++) {
 					if(P.collidesWith(lv.getDestructable(n)) && !explode && lv.getDestructable(n).getVisible() == true) {
+						SM.playSoundEffect("platdestroyed");
 						this.dispatchEvent(new ProjectileEvent(ProjectileEvent.PROJECTILE_EXPLODE,P,P.getId(),"s"));
 						explode = true;
 						lv.getDestructable(n).setVisible(false);
@@ -275,6 +291,7 @@ public class Prototype extends Game {
 			TJ.nextFrame();
 		}
 		else {
+			SM.playMusic("firstscreen");
 			if(!this.startMenu.getPlayerSelect()) {
 				if(pressedKeys.contains(KeyEvent.VK_SPACE)) {
 					this.startMenu.setPlayerSelect(true);
@@ -283,32 +300,43 @@ public class Prototype extends Game {
 				}
 			}
 			if(pressedKeys.contains(KeyEvent.VK_2) && this.startMenu.getPlayerSelect()) {
+				SM.playSoundEffect("MenuSelect");
 				this.startMenu.setNumPlayers(2);
 			}
 			if(pressedKeys.contains(KeyEvent.VK_3) && this.startMenu.getPlayerSelect()) {
+				SM.playSoundEffect("MenuSelect");
 				this.startMenu.setNumPlayers(3);
 			}
 			if(pressedKeys.contains(KeyEvent.VK_4) && this.startMenu.getPlayerSelect()) {
+				SM.playSoundEffect("MenuSelect");
 				this.startMenu.setNumPlayers(4);
 			}
 			if(pressedKeys.contains(KeyEvent.VK_F) && this.startMenu.getPlayerSelect()) {
+				SM.playSoundEffect("MenuSelect");
 				this.startMenu.setLevel(1);
 			}
 			if(pressedKeys.contains(KeyEvent.VK_D) && this.startMenu.getPlayerSelect()) {
+				SM.playSoundEffect("MenuSelect");
 				this.startMenu.setLevel(2);
 			}
 			if(pressedKeys.contains(KeyEvent.VK_W) && this.startMenu.getPlayerSelect()) {
+				SM.playSoundEffect("MenuSelect");
 				this.startMenu.setLevel(3);
 			}
  			if(pressedKeys.contains(KeyEvent.VK_ENTER) && this.startMenu.getPlayerSelect()) {
+				SM.playSoundEffect("GO");
+				SM.stopMusic("firstscreen");
 				switch (this.startMenu.getLevel()) {
 				case 1:
 					lv = new Level1("lv1", "Background_field.png", this.startMenu.getNumPlayers());
+					SM.playMusic("fieldmusic");
 					break;
 				case 2:
+					SM.playMusic("desertmusic");
 					lv = new Level2("lv2", "Background_desert.png", this.startMenu.getNumPlayers());
 					break;
 				default:
+					SM.playMusic("wintermusic");
 					lv = new Level3("lv3", "Background_winternight.png", this.startMenu.getNumPlayers());
 				}
 				this.addChild(lv.getBackground());
@@ -460,7 +488,13 @@ public class Prototype extends Game {
 			}
 		}
 		if(end) {
+			SM.playSoundEffect("Victory");
+			//SM.stopMusic("fieldmusic");
 			Winner = String.format("%s WINS!!", Winner);
+			//Ending_Instructions = String.format("Please Exit and Restart the Game to Play Again!", 0);
+			//g.drawString(indented_instructions, 0, 600);
+			g.drawString(Ending_Instructions, 100, 600);
+			g.drawString(Ending_Instructions2, 100, 700);
 			g.drawString(Winner, 400, 400);
 			this.dispatchEvent(new GameEvent(GameEvent.GAME_OVER,this));
 		}
